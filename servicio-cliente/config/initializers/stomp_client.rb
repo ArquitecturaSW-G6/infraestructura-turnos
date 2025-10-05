aa46f28f6de54e2e280e5578407fecf655841442
00CARLOSECG00 <caicedog.ce@javeriana.edu.co>
2025-10-05 18:21:11 -0500

    Commit para nueva-version

--- servicio-cliente/config/initializers/stomp_client.rb
+++
@@ -1,9 +0,0 @@
-require 'stomp'
-
-class StompClient
-  def self.publish(queue, message)
-    client = Stomp::Client.new("tcp://admin:admin@activemq:61613")
-    client.publish(queue, message)
-    client.close
-  end
-end
